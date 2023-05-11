1d2a045d0273e15e914a3173e4de48daac60f745
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-05-11 23:29:21 +0200

    minimal repro code

--- src/components/prompt.tsx
+++
@@ -1,9 +0,0 @@
-import { Attrs } from "xania";
-
-export function Prompt() {
-  return (
-    <>
-      <Attrs class="border-2 border-solid border-amber-500" />
-    </>
-  );
-}
--- src/components/search.tsx
+++
@@ -1,9 +0,0 @@
-import { Attrs } from "xania";
-
-export function Search() {
-  return (
-    <>
-      <Attrs class="border-2 border-solid border-amber-500" />
-    </>
-  );
-}
