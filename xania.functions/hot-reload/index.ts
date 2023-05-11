931488eb051519f59202f32102b46e16c2bd808a
Ibrahim ben Salah <ibrahim.bensalah@xania.nl>
2023-05-11 23:17:19 +0200

    wip

---
+++ xania.functions/hot-reload/index.ts
@@ -0,0 +1,24 @@
+import { AzureFunction, Context } from "@azure/functions";
+import { promises as fs } from "fs";
+
+const timerTrigger: AzureFunction = async function (
+  context: Context,
+  myTimer: any
+): Promise<void> {
+  touch("../src/index.tsx");
+};
+
+export default timerTrigger;
+
+async function touch(filename: string) {
+  console.log("touch", filename);
+  const time = new Date();
+
+  await fs.utimes(filename, time, time).catch(async function (err) {
+    if ("ENOENT" !== err.code) {
+      throw err;
+    }
+    let fh = await fs.open(filename, "a");
+    await fh.close();
+  });
+}
---
+++ xania.functions/mollie/index.ts
@@ -0,0 +1,63 @@
+import { Context, HttpRequest } from "@azure/functions";
+import createMollieClient from "@mollie/api-client";
+import { section } from "../config";
+
+export default async function httpTrigger(
+  context: Context,
+  req: HttpRequest
+): Promise<void> {
+  const config = section(req.params.client, {
+    mollie: {
+      apiKey: null,
+      redirectUrl: req.headers["referer"],
+    },
+  });
+
+  if (config instanceof Error) {
+    context.res = {
+      status: 500,
+      body: config.message,
+    };
+    return;
+  }
+
+  var rawBody = req.rawBody as string;
+  if (rawBody) {
+    var fields = rawBody.split("&").reduce((prev, curr) => {
+      const [key, value] = curr
+        .split("=")
+        .map((s) => s.replace("+", " "))
+        .map(decodeURIComponent)
+        .map(decodeURI);
+      if (prev) {
+        if (prev[key]) {
+          prev[key] += ", " + value;
+        } else {
+          prev[key] = value;
+        }
+        return prev;
+      } else {
+        return { [key]: value };
+      }
+    }, {} as { [k: string]: string });
+
+    var mollieClient = createMollieClient({ apiKey: config.mollie.apiKey });
+
+    var payment = await mollieClient.payments.create({
+      description: fields["Description"],
+      amount: {
+        currency: fields.Currency,
+        value: fields.Amount,
+      },
+      redirectUrl: config.mollie.redirectUrl,
+    });
+
+    context.res = {
+      status: 302,
+      headers: {
+        Location: payment._links.checkout?.href,
+      },
+    };
+    return;
+  }
+}
